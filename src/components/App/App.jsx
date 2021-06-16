babd72b5b55e8d20f9b7b9d5c3f84b5beddbc625
Siddhant <siddhant.roy2019@vitstudent.ac.in>
2021-06-16 14:16:52 +0530

    NavBar completed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,12 +6,14 @@ import Projects from "./../Projects/Projects";
 import Skills from "./../Skills/Skills";
 import Contact from "./../Contact/Contact";
 import "./App.css";
+import NavBar from "../NavBar/NavBar";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+          <NavBar />
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/projects" component={Projects} />
