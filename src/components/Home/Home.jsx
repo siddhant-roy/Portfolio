181a7334c52665ded1a1a0586e7b85604437a7d1
Siddhant <siddhant.roy2019@vitstudent.ac.in>
2021-06-18 03:26:49 +0530

    Added Modal

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import Modal from "../Modal/Modal";
 import homeAnime from "./../../assets/home_anime.gif";
 import "./Home.css";
 
 class Home extends Component {
+  state = { showModal: false };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ showModal: true });
+    }, 1000);
+  }
+
+  closeModal = () => {
+    this.setState({ showModal: false });
+  };
+
   render() {
     return (
       <div className="home-container">
+        {this.state.showModal && <Modal closeModal={this.closeModal} />}
         <div className="header-text">
           <h1>Welcome to my portfolio!</h1>
-          <br />
           <p>I'm Siddhant, a full stack developer</p>
         </div>
         <div className="head-btns">
