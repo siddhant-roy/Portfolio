4bb5272e8fe82ead15297bab600365d62ae7eae6
Siddhant <siddhant.roy2019@vitstudent.ac.in>
2021-06-18 05:03:16 +0530

    Added Skills page

--- src/assets/skillsData.js
+++ src/assets/skillsData.js
@@ -1,42 +1,49 @@
 export const skillList = [
-    {
-        skillName: 'HTML',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/html.png',
-    },
-    {
-        skillName: 'CSS',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/css.png',
-    },
-    {
-        skillName: 'JavaScript',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/js.png',
-    },
-    {
-        skillName: 'React',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/react.png',
-    },
-    {
-        skillName: 'Redux',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/redux.png',
-    },
-    {
-        skillName: 'NodeJS',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/node.png',
-    },
-    {
-        skillName: 'TypeScript',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/ts.png',
-    },
-    {
-        skillName: 'Java',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/java.png',
-    },
-    {
-        skillName: 'C#',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/cs.png',
-    },
-    {
-        skillName: 'MongoDB',
-        skillUrl: 'https://madhavbahl.tech/portfolio-assets/mongo.png',
-    },
+  {
+    skillName: "HTML",
+    skillUrl:
+      "https://raw.githubusercontent.com/siddhant-roy/Portfolio/main/src/assets/html.png",
+  },
+  {
+    skillName: "CSS",
+    skillUrl:
+      "https://raw.githubusercontent.com/siddhant-roy/Portfolio/main/src/assets/css.png",
+  },
+  {
+    skillName: "JavaScript",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/js.png",
+  },
+  {
+    skillName: "React",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/react.png",
+  },
+  {
+    skillName: "Redux",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/redux.png",
+  },
+  /*{
+    skillName: "NodeJS",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/node.png",
+  },*/
+  {
+    skillName: "TypeScript",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/ts.png",
+  },
+  {
+    skillName: "Java",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/java.png",
+  },
+  {
+    skillName: "C#",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/cs.png",
+  },
+  {
+    skillName: "SQL",
+    skillUrl:
+      "https://raw.githubusercontent.com/siddhant-roy/Portfolio/main/src/assets/ms-sql-server.png",
+  },
+  {
+    skillName: "MongoDB",
+    skillUrl: "https://madhavbahl.tech/portfolio-assets/mongo.png",
+  },
 ];
